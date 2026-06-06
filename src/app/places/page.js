266602abf546d9cb01027eb7e16f58518a971f36
Ganeshmurthy-G <ganeshmurthy.g@mitrahsoft.in>
2026-06-06 13:52:37 +0530

    Added place and location in places

--- src/app/places/page.js
+++ src/app/places/page.js
@@ -33,6 +33,10 @@ export default function PlacesPage() {
                 {place.location}
               </p>
 
+              <p className="text-gray-600 mt-2">
+                {place.name},{place.location}
+              </p>
+
               <Link
                 href={`/places/${place.id}`}
                 className="inline-block mt-4 bg-blue-500 text-white px-4 py-2 rounded"
