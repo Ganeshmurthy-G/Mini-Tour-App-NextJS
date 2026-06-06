dfa0c0476db2fa586f0842c7b55b22937f06521c
Ganeshmurthy-G <ganeshmurthy.g@mitrahsoft.in>
2026-06-06 13:38:00 +0530

    Intentionally made a mistake in test file

--- src/app/Tests/Home.test.js
+++ src/app/Tests/Home.test.js
@@ -6,7 +6,7 @@ describe("Home Page", () => {
     render(<Home />);
 
     expect(
-      screen.getByText("MINI TOUR GUIDE")
+      screen.getByText("MINI TOUR GUIDE123")
     ).toBeInTheDocument();
   });
 
