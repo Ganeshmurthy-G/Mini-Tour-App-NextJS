8f58be815ca4ce7d0ee596e3a821e2437c9b25f2
Ganeshmurthy-G <ganeshmurthy.g@mitrahsoft.in>
2026-06-06 13:32:47 +0530

    Added Test.yml and test files

---
+++ src/app/Mocks/nextImageMock.js
@@ -0,0 +1,5 @@
+const NextImage = (props) => {
+  return <img {...props} alt={props.alt} />;
+};
+
+export default NextImage;
---
+++ src/app/Tests/Home.test.js
@@ -0,0 +1,30 @@
+import { render, screen } from "@testing-library/react";
+import Home from "../page";
+
+describe("Home Page", () => {
+  test("renders main heading", () => {
+    render(<Home />);
+
+    expect(
+      screen.getByText("MINI TOUR GUIDE")
+    ).toBeInTheDocument();
+  });
+
+  test("renders guide description", () => {
+    render(<Home />);
+
+    expect(
+      screen.getByText("I will guide you to amazing places")
+    ).toBeInTheDocument();
+  });
+
+  test("renders button", () => {
+    render(<Home />);
+
+    expect(
+      screen.getByRole("button", {
+        name: /Choose Your Tour/i,
+      })
+    ).toBeInTheDocument();
+  });
+});
