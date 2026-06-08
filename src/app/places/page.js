591d2b1b6e9ba89d60c62cfbc529d3882188ee72
Ganeshmurthy-G <ganeshmurthy.g@mitrahsoft.in>
2026-06-08 13:12:34 +0530

    Added a paragaph in places

--- src/app/places/page.js
+++ src/app/places/page.js
@@ -37,6 +37,8 @@ export default function PlacesPage() {
                 {place.name},{place.location}
               </p>
 
+              <p>Have a goodtime!</p>
+
               <Link
                 href={`/places/${place.id}`}
                 className="inline-block mt-4 bg-blue-500 text-white px-4 py-2 rounded"
