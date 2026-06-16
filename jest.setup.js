e9f1ed2264246b4986f0f40e4946635903d0c9b9
Ganeshmurthy-G <ganeshmurthy.g@mitrahsoft.in>
2026-06-16 11:46:17 +0530

    Fixed Failed Test

--- jest.setup.js
+++ jest.setup.js
@@ -1,8 +1,12 @@
 import "@testing-library/jest-dom";
 
+// Set environment variables for testing
+process.env.NEXT_PUBLIC_APP_NAME = "Mini Tourist App";
+
 jest.mock("next/image", () => ({
   __esModule: true,
   default: (props) => {
-    return <img {...props} alt={props.alt} />;
+    const { fill, priority, ...rest } = props;
+    return <img {...rest} alt={props.alt} />;
   },
 }));
